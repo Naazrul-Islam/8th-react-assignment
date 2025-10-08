7a60631d92752b0695731f7f1341a3bebed432df
Naazrul-Islam <hmnazrulislam06@gmail.com>
2025-10-08 03:50:21 -0700

    some style added

--- src/Page/DetailsApplication.jsx
+++ src/Page/DetailsApplication.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useParams } from "react-router";
 import useApplication from "../coponents/useApplication";
+import DetailsTop from "../coponents/DetailsTop";
 
 const DetailsApplication = () => {
   const { id } = useParams();
@@ -14,40 +15,7 @@ const DetailsApplication = () => {
   );
 
   return (
-    <div className="max-w-[1440px] mx-auto">
-      <div className="flex my-20 gap-20">
-        <div>
-          <img src="/clock.png" alt="" className="lg:w-[300px] w-[200px] flex justify-center items-center my-10" />
-        </div>
-        <div>
-          <div className="my-10">
-            <h1 className="text-4xl font-bold">SmPlan:ToDo List with Reminder</h1>
-            <p className="text-gray-400 text-xl">Developed by productive.io</p>
-          </div>
-          <hr />
-          <div className="flex my-10 gap-20 ">
-            <div className="">
-              <img src="/icon.png" alt="" className="w-[40px]" />
-              <p>Downloads</p>
-              <h1 className="text-4xl font-bold">8M</h1>
-            </div>
-            <div>
-              <img src="/iconR.png" alt="" className="w-[40px]" />
-              <p>Average Ratings</p>
-              <h1 className="text-4xl font-bold">8M</h1>
-            </div>
-            <div>
-              <img src="/iconRe.png" alt="" className="w-[40px]" />
-              <p>Total Revews</p>
-              <h1 className="text-4xl font-bold">8M</h1>
-            </div>
-          </div>
-          <Link  className="bg-[linear-gradient(90deg,#632EE3_0%,#9F62F2_100%)] text-white px-4 py-2 rounded-lg my-10 items-center" >Install Now (291 MB)</Link>
-        </div>
-        
-      </div>
-
-    </div>
+    <DetailsTop></DetailsTop>
   );
 };
 
--- src/coponents/Navber.jsx
+++ src/coponents/Navber.jsx
@@ -29,13 +29,13 @@ const Navber = () => {
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
             <li>
-              <NavLink>Home</NavLink>
+              <NavLink to="/">Home</NavLink>
             </li>
             <li>
-              <NavLink>Apps</NavLink>
+              <NavLink to="/apps">Apps</NavLink>
             </li>
             <li>
-              <NavLink>Installation</NavLink>
+              <NavLink to="/installation">Installation</NavLink>
             </li>
           </ul>
         </div>
